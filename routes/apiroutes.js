7caaedd6f3d679848b6c3e091f59502115c0b6de
Tadhg McIntyre <mcintyretadhg@gmail.com>
2021-04-09 10:23:11 -0400

    added functionality to delete notes. and moved routes to their own files

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,13 +1,27 @@
-const path = require('path');
 const db = require('../db/db.json');
+const Note = require('../lib/Note');
 
 module.exports = (app) => {
     app.get('/api/notes', (req, res) => res.json(db));
 
     app.post('/api/notes', (req, res) => {
-        //input code here that adds the post to the data base
-        const newNote = req.body;
-        db.push(newNote);
+        let request = req.body;
+        let note = new Note(request);
+        db.push(note);
+        console.log(note);
         res.json(true);
-    })
+    });
+
+    app.delete('/api/notes/:id', (req, res) =>{
+        const specificNote = req.params.id;
+        console.log(specificNote);
+        for (let i = 0; i < db.length; i++){
+            if (specificNote === db[i].id){
+                db.splice(i, 1);
+                console.log('deleted note!');
+                return res.json(true)
+            }
+        } 
+        return console.log("Couldn't find that note");
+    });
 }
